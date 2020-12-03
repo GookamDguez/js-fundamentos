57170e11124a0eb98cfc20d61c8d45b93b6512aa
Gaby Dominguez <ookamimetalfan@gmail.com>
2020-12-03 00:59:35 -0600

    modificando sangria

--- proyect-Simon-Dice/funciones.js
+++ proyect-Simon-Dice/funciones.js
@@ -3,16 +3,17 @@ const naranja = document.getElementById('naranja')
 const verde = document.getElementById('verde')
 const btnEmpezar = document.getElementById('btnEmpezar')
 const celeste = document.getElementById('celeste')
-      class Juego {
+
+    class Juego {
         constructor() {
-          this.inicializar()
+            this.inicializar()
         }
 
         inicializar() {
-          btnEmpezar.classList.add('hide')
+            btnEmpezar.classList.add('hide')
         }
-      }
+    }
 
-      function empezarJuego() {
+    function empezarJuego() {
         var juego = new Juego()
-      }
+    }
